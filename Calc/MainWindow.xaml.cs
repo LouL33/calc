d5fb4a7599d7f5b49626994536bcef15c8443fa9
Lou Lee <Lou7532@hotmail.com>
2017-04-03 00:13:45 -0400

    lvl 2 almost

--- Calc/MainWindow.xaml.cs
+++ Calc/MainWindow.xaml.cs
@@ -19,62 +19,78 @@ namespace Calc
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    { 
+    {
+            decimal MemoryOutputs = 0;
+            decimal MemoryEndResult = 0;
         
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Add_Click(object sender, RoutedEventArgs e)
+       
+        //number pad on the calc
+        private void TheNumButtons(object sender, RoutedEventArgs e)
         {
-
-           var myNumber = int.Parse(input.Text) + int.Parse(input2.Text);
-            AnswerDisplay.Text = myNumber.ToString();
-
+            input.Text += (string)((Button)sender).Content;
         }
-        private void Subtrack_Click(object sender, RoutedEventArgs e)
-        {
-            var myNumber = int.Parse(input.Text) - int.Parse(input2.Text);
-            AnswerDisplay.Text = myNumber.ToString();
-
-        }
-
-        private void Multi_Click_1(object sender, RoutedEventArgs e)
-        {
-            var myNumber = int.Parse(input.Text) * int.Parse(input2.Text);
-            AnswerDisplay.Text = myNumber.ToString();
-        }
-
-        private void Divid_Click(object sender, RoutedEventArgs e)
-        {
-            var myNumber = int.Parse(input.Text) / int.Parse(input2.Text);
-            AnswerDisplay.Text = myNumber.ToString();
-
-        }
-
-        private void Percent_Click(object sender, RoutedEventArgs e)
-        {
-            var myNumber = int.Parse(input.Text) % int.Parse(input2.Text);
-            AnswerDisplay.Text = myNumber.ToString();
-        }
-
+        // textbox
         private void input_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
-
-        private void input2_TextChanged(object sender, TextChangedEventArgs e)
+        // all number buttons
+        private void CLick_Click(object sender, RoutedEventArgs e)
         {
+            input.Text += " " + (string)((Button)sender).Content + " ";
+        }
+        // euqals button
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            string[] text = input.Text.Split(' ');
 
+                double n1 = double.Parse(text[0]);
+                double n2 = double.Parse(text[2]);
 
+            if(text[1] == "+")
+            {
+                input.Text = (n1 + n2).ToString();
+            }
+            else if (text[1] == "-")
+            {
+                input.Text = (n1 - n2).ToString();
+            }
+            else if (text[1] == "/")
+            {
+                if (n2 == 0.0)
+                    input.Text = "why are we friends";
+                else
+                    input.Text = (n1 / n2).ToString();
+            }
+            else if (text[1] == "*")
+            {
+                input.Text = (n1 * n2).ToString();
+            }
+        }
+        // clear button
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = "";
         }
 
-        private void AnswerDisplay_TextChanged(object sender, TextChangedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-
+            input.Text = input.Text.Remove(input.Text.Length - 1, 1);
         }
 
+
+        private void Memory_Click(object sender, RoutedEventArgs e)
+        {
+
+            Button MemoryButtonsPushed = (Button)sender;
+            string MemButtonTxt = MemoryButtonsPushed.Text;
+
+        }
     }
 }
